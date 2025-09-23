e803db6ef8ae4458f14abbf447ae0a3c0a634148
liuxiaorui <1078937580@qq.com>
2025-09-23 10:23:49 +0800

    fix: 调整图片的位置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -61,7 +61,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/your-username/component-intro' }
+      { icon: 'github', link: 'https://github.com/xiliubianbian/component-intro/tree/main' }
     ],
 
     footer: {
